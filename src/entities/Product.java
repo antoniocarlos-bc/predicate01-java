73fa7bc6150369ecce429277a4d414f78dd61d40
antoniocarlos-bc <antoniocarlos_bc@outlook.com>
2020-12-26 11:34:48 -0300

    Predicate with method no static

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -27,8 +27,8 @@ public class Product {
 	}
 
 	
-	public static boolean staticProductPredicate(Product p) {
-		return p.getPrice()>=100.00;
+	public boolean nonStaticProductPredicate() {
+		return price >= 100.00;
 	}
 	
 	@Override
